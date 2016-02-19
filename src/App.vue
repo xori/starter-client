504033bbe62df4fe177356e8b9b1225d35729acb
Evan Verworn <evan@verworn.net>
2016-02-18 23:26:30 -0500

    Bootstrap Added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <logo></logo>
     <h1><i class="material-icons">flash_on</i> {{ msg }} <i class="material-icons">flash_on</i></h1>
     <p>
       <!-- use v-link directive for navigation. -->
-      <a v-link="{ path: '/foo' }">Go to Foo</a>
-      <a v-link="{ path: '/bar' }">Go to Bar</a>
+      <a class="btn btn-primary" v-link="{ path: '/foo' }">Go to Foo</a>
+      <a class="btn btn-default" v-link="{ path: '/bar' }">Go to Bar</a>
     </p>
     <!-- route outlet -->
     <router-view></router-view>
