ca3ef8d2d4f0b921b935d96b8f7bb6e49c87610d
Evan Verworn <evan.verworn@dsbn.org>
2016-02-22 16:31:48 -0500

    autorestart when server changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  req.db.all(() => {
+    return true;
+  }, (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
+router.get('/insert', function (req, res) {
+  req.db.insert({bookmark: +new Date()}, (err, count) => {
+    res.send('inserted ' + count + '<br>' + err);
+  });
 });
 
 module.exports = router;
